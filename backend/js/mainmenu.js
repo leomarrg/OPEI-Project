fa6309dd514fe960d4584c56c47293286f7e8a13
JaredPupo <jared.pupo@upr.edu>
2024-03-01 19:52:42 -0400

    Main menu now shows which department it displays

--- backend/js/mainmenu.js
+++ backend/js/mainmenu.js
@@ -1,4 +1,4 @@
-function showOptions(className, element) {
+function showOptions(className, setName, element) {
     var options = document.querySelectorAll('.menu-container');
     options.forEach(function(option) {
         option.style.display = 'none';
@@ -11,8 +11,11 @@ function showOptions(className, element) {
 
     document.querySelector('#welcome-text').style.display = 'none';
     document.querySelector('#year-text').style.display = 'block';
+    document.querySelector('#active-set-text').style.display = 'block';
     document.querySelector('.' + className).style.display = 'flex';
 
     // Highlight the clicked option
     element.classList.add('highlight');
+
+    document.querySelector('#active-set-text').innerText = setName;
 }

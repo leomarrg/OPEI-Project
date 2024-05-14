fcc61a5934990fa030183dfb73a632d1a50a69da
leomarrg <31631486+leomarrg@users.noreply.github.com>
2024-05-14 18:06:56 -0400

    Update tabla1.2.php

--- opei-project-front-end-v2/opei-project-front-end/meta-1/tabla1.2.php
+++ opei-project-front-end-v2/opei-project-front-end/meta-1/tabla1.2.php
@@ -411,11 +411,5 @@
         <!-- Content for the footer box goes here -->
         <p>&copy; 2024 UPRA Reports. All rights reserved.</p>
     </div>
-
-    <script src="../js/jquery-3.3.1.min.js"></script>
-    <script src="../js/popper.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/jquery.validate.min.js"></script>
-    <script src="../js/main.js"></script>
 </body>
 </html>

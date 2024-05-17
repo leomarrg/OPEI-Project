7a750cb64336094fd9a730070342494055e9d7b5
JaredPupo <jared.pupo@upr.edu>
2024-05-17 12:17:25 -0400

    Update to let aixa enter the DB

--- opei-project-phase-3/opei-database.sql
+++ opei-project-phase-3/opei-database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2024 at 01:12 PM
+-- Generation Time: May 17, 2024 at 06:15 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,7 +42,9 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`adminID`, `Email`, `Password`, `Active`, `Name`, `LastName`) VALUES
 (1, 'jared.pupo@upr.edu', '$2y$10$T3KV8X/kQvbdenDygNZPu.iIUB83LX0CVFdWal4wbdBPJaytmoWD.', 1, 'Jared', 'Jared'),
-(2, 'admin.admin@upr.edu', '$2y$10$KNzRtUuNThqWtTjVueLb2ONJq7WLSo6aOP7YecLRDcb4NSP7TiMeq', 1, 'admin', 'admin');
+(2, 'admin.admin@upr.edu', '$2y$10$KNzRtUuNThqWtTjVueLb2ONJq7WLSo6aOP7YecLRDcb4NSP7TiMeq', 1, 'admin', 'admin'),
+(3, 'email@upr.edu', '$2y$10$PAC8htZFzozf68wW.c.JgeEb359o7I3s9QLy0AAjaA4siGkouS9SO', 1, 'Name', 'Last name'),
+(4, 'aixa.ramirez@upr.edu', '$2y$10$HIHBHvgE7So3UEzjAHfhTub1t88SDvo/RpebeLAc8m26v65Yl5yZe', 1, 'Aixa', 'Ramirez Toledo');
 
 -- --------------------------------------------------------
 
@@ -92,8 +94,8 @@ CREATE TABLE `table11a` (
 --
 
 INSERT INTO `table11a` (`table11aID`, `DepartmentID`, `year`, `field1`, `field2`, `field3`, `field4`) VALUES
-(13, 2, 2024, 'CCOM3001', 'Activación', 'Aprobado', 'Una breve descripción'),
-(21, 1, 2024, 'CCOM4095', 'Activación', 'Aprobado', 'Proyecto Final');
+(13, 2, 2024, 'ADEM0000', 'Activación', 'Aprobado', 'Una breve descripción'),
+(23, 1, 2024, 'CCOM4005', 'Activación', 'Aprobado', '12');
 
 -- --------------------------------------------------------
 
@@ -117,7 +119,7 @@ CREATE TABLE `table11b` (
 
 INSERT INTO `table11b` (`table11bID`, `DepartmentID`, `year`, `field1`, `field2`, `field3`, `field4`) VALUES
 (19, 2, 2024, 'Valor Cambio académico', 'Valor Academico Institucional', 'Valor Cambio Academico', 'Valor Cambio Sustanciales'),
-(21, 1, 2024, 'Valor para cambio academico', 'Valor Academico Institucional', 'Valor Cambio Academico', 'Valor Cambio Sustancial');
+(21, 1, 2024, 'Valor para cambio academicos', 'Valor Academico Institucionales', 'Valor Cambio Academicos', 'Valor Cambio Sustanciales');
 
 -- --------------------------------------------------------
 
@@ -154,7 +156,8 @@ CREATE TABLE `table12` (
 --
 
 INSERT INTO `table12` (`table12ID`, `DepartmentID`, `year`, `field1`, `field2`, `field3`, `field4`, `field5`, `field6`, `field7`, `field8`, `field9`, `field10`, `field11`, `field12`, `field13`, `field14`, `field15`, `field16`, `field17`, `field18`) VALUES
-(41, 1, 2024, '2', '7', '5', '18', '14', '11', '10', '10', '12', '1', '9', '16', '40', '36', '41', '56', '15', '58');
+(42, 1, 2024, '1', '2', '3', '53', '15', '34', '15', '54', '41', '38', '6', '30', '13', '10', '3', '12', '13', '11'),
+(43, 2, 2024, '12', '4', '60', '53', '13', '12', '15', '11', '45', '23', '43', '14', '42', '60', '51', '2', '16', '5');
 
 -- --------------------------------------------------------
 
@@ -179,8 +182,8 @@ CREATE TABLE `table13` (
 --
 
 INSERT INTO `table13` (`table13ID`, `DepartmentID`, `year`, `field1`, `field2`, `field3`, `field4`, `field5`, `field6`) VALUES
-(32, 2, 2024, 'CCOM3002', 'Agencia que acredito el programa', '2023', '2024', '2', 'Recomendación'),
-(33, 1, 2024, 'CCOM3001', 'Agencia que acredito el programa', '2023', '2024', '4', 'Recomendación');
+(32, 2, 2024, 'ADEM1000', 'Agencia que acredito el programa', '2023', '2024', '2', 'Recomendación'),
+(33, 1, 2024, 'CCOM3010', 'Agencia que acredito el programas', '2024', '2025', '2', 'Recomendaciónes aqui');
 
 -- --------------------------------------------------------
 
@@ -204,8 +207,7 @@ CREATE TABLE `table14a` (
 --
 
 INSERT INTO `table14a` (`table14aID`, `DepartmentID`, `year`, `field1`, `field2`, `field3`, `field4`, `field5`) VALUES
-(4, 2, 2024, 'Value1', 'Value1_2', 'Value1_3', 'Value1_4', 'Value1_5asdasdasd'),
-(10, 1, 2024, 'CCOM4065', 'Estrategia', 'Indicador', 'Resultados', 'Acciones que son correctivas');
+(4, 2, 2024, 'Value1', 'Value1_2', 'Value1_3', 'Value1_4', 'Value1_5asdasdasd');
 
 -- --------------------------------------------------------
 
@@ -256,7 +258,7 @@ CREATE TABLE `table15` (
 
 INSERT INTO `table15` (`table15ID`, `DepartmentID`, `year`, `field1`, `field2`, `field3`, `field4`, `field5`, `field6`) VALUES
 (7, 2, 2024, '1.1 Review', '1.2 Review', '1.3 Review', '1.4 Review', '1.5 Review', '1.6 Review'),
-(8, 1, 2024, '1.1 Reviews', '1.2 Reviews', '1.3 Review', '1.4 Reviews', '1.5 Reviews', '1.6 Reviews');
+(9, 1, 2024, '1.1 Review', '1.2 Review', '1.3 Review', '1.4 Review', '1.5 Review', '1.6 Review');
 
 -- --------------------------------------------------------
 
@@ -283,8 +285,7 @@ CREATE TABLE `table21a` (
 --
 
 INSERT INTO `table21a` (`table21aID`, `DepartmentID`, `year`, `field1`, `field2`, `field3`, `field4`, `field5`, `field6`, `field7`, `field8`) VALUES
-(21, 2, 2024, 'Insertar Profesor', 'Actividad', '11-05-2024', 'Si', 'Libro', 'Arbritada', 'Si', 'Entidad'),
-(23, 1, 2024, 'Insertar Profesor', 'Actividad', '18-05-2024', 'No', 'Artículo Publicado Revista Arbitrada', 'No arbritada', 'No', 'Entidad');
+(21, 2, 2024, 'Insertar Profesor', 'Actividad', '11-05-2024', 'Si', 'Libro', 'Arbritada', 'Si', 'Entidad');
 
 -- --------------------------------------------------------
 
@@ -433,7 +434,7 @@ CREATE TABLE `table31` (
 
 INSERT INTO `table31` (`table31ID`, `DepartmentID`, `year`, `field1`, `field2`, `field3`, `field4`, `field5`) VALUES
 (12, 2, 2024, 'Artístico', '10', 'Reconocimientos descriptivos', '11-05-2024', 'El organismo'),
-(14, 1, 2024, 'Artístico', '10', 'Reconocimientos descriptivos', '2024-05-17', 'El organismo');
+(14, 1, 2024, 'Artístico', '10', 'Reconocimientos descriptivos', 'Words', 'as');
 
 -- --------------------------------------------------------
 
@@ -482,7 +483,7 @@ CREATE TABLE `table32b` (
 
 INSERT INTO `table32b` (`table32bID`, `DepartmentID`, `year`, `field1`, `field2`, `field3`, `field4`, `field5`, `field6`) VALUES
 (8, 2, 2024, 'Estudiante', 'CCOM', 'Algun lugar', '2024', 'Si', 'Comentado'),
-(11, 1, 2024, 'Estudiante', 'CCOM', 'Algun lugar', '2024', 'Si', 'Comentado');
+(11, 1, 2024, 'Estudiante', 'CCOM', 'Algun lugar', '2024', 'aa', 'aa');
 
 -- --------------------------------------------------------
 
@@ -507,7 +508,7 @@ CREATE TABLE `table33` (
 
 INSERT INTO `table33` (`table33ID`, `DepartmentID`, `year`, `field1`, `field2`, `field3`, `field4`, `field5`) VALUES
 (6, 2, 2024, '3.1 Review', '3.2 Review', '3.3 Review', '3.4 Review', '3.5 Review'),
-(8, 1, 2024, 'Review 3.1', 'Review 3.2', 'Review 3.3', 'Review 3.4', 'Review 3.5');
+(8, 1, 2024, 'Review 3.1s', 'Review 3.2s', 'Review 3.3s', 'Review 3.4s', 'Review 3.5s');
 
 -- --------------------------------------------------------
 
@@ -709,7 +710,9 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`UserID`, `Email`, `Password`, `DepartmentID`, `Name`, `LastName`, `Active`) VALUES
 (4, 'Jared.User@upr.edu', '$2y$10$B9yOrGDrSQeG8aZj64Nq0.juzUgiUq8//jgSZA9gPB/TtFAnQzhz2', 1, 'Jared', 'Pupo', 1),
-(12, 'user.adem@upr.edu', '$2y$10$9/URjLjK6yBimsPRF/D5Mu1IensTNK9KzVkByFBAF9kZHjIU0tvy6', 2, 'ADEM', 'ADEM', 1);
+(12, 'user.adem@upr.edu', '$2y$10$9/URjLjK6yBimsPRF/D5Mu1IensTNK9KzVkByFBAF9kZHjIU0tvy6', 2, 'ADEM', 'ADEM', 1),
+(13, 'jared@gmail.com', '$2y$10$8uGB5We1OUofiLCm4eTyeON8g5NlaC0X.ogeWgkrRaNh0n3gPrYwO', 4, 'Jared', 'Jared', 1),
+(14, 'aixa.user@upr.edu', '$2y$10$Fcnn/wSUL5YW3FnTiyLWReX5A8eV9PuuE970wapmMaFpkhXF9PB92', 1, 'Aixa', 'Usuario', 1);
 
 --
 -- Indexes for dumped tables
@@ -879,13 +882,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `adminID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `adminID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `table11a`
 --
 ALTER TABLE `table11a`
-  MODIFY `table11aID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `table11aID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `table11b`
@@ -897,7 +900,7 @@ ALTER TABLE `table11b`
 -- AUTO_INCREMENT for table `table12`
 --
 ALTER TABLE `table12`
-  MODIFY `table12ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `table12ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `table13`
@@ -915,13 +918,13 @@ ALTER TABLE `table14a`
 -- AUTO_INCREMENT for table `table14b`
 --
 ALTER TABLE `table14b`
-  MODIFY `table14bID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `table14bID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `table15`
 --
 ALTER TABLE `table15`
-  MODIFY `table15ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `table15ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `table21a`
@@ -1029,7 +1032,7 @@ ALTER TABLE `table54`
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
